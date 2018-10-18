c81cf0eb3fb90d3c3ad26e3c0dac96a8454550b3
Steve Sixty-Four <steve8x8@googlemail.com>
2018-10-18 19:46:16 +0200

    Release 3.28.3

--- geotoad.rb
+++ geotoad.rb
@@ -1,8 +1,8 @@
 class Geotoad < Formula
   desc "Query tool to query the geocaching.com website"
   homepage "https://github.com/steve8x8/geotoad"
-  url "https://github.com/steve8x8/geotoad/archive/3.28.2.tar.gz"
-  sha256 "89ff723c42554bae2ceedcd174706367d0a8e57e9120a1a46974d8f87e92f0e1"
+  url "https://github.com/steve8x8/geotoad/archive/3.28.3.tar.gz"
+  sha256 "2e9927264e4b86ae43ffd5f6e29d2018e9e43016a02474ff117efcea6382562e"
   head "https://github.com/steve8x8/geotoad.git"
 
   def install
